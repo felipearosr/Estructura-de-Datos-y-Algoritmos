4ec9488f36c7892c814f65a00af619822dde8abb
Felipe Aros <felipearos@pop-os.localdomain>
2020-05-12 01:46:28 -0400

    .

--- laboratorios/lab3/HeapSort.java
+++ laboratorios/lab3/HeapSort.java
@@ -1,15 +1,46 @@
-import sun.jvm.hotspot.oops.Array;
-
 public class HeapSort {
 
-    public static int Sort(int arr[]) {
+    public void Sort(int arr[]) {
         int length = arr.length;
-
+        for (int i = length / 2 - 1; i >= 0; --i)
+        Heapify(arr, length, i);
+        for (int i = length - 1; i > 0; i--)  { 
+            Swap(arr, 0, i);
+            Heapify(arr, i, 0); 
+        } 
     }
 
+    void Heapify(int arr[], int length, int i) {
+        int largest = i,
+        left = 2 * i + 1,
+        right = 2 * i + 2;
+        if (left < length && arr[left] > arr[largest]) 
+        largest = left; 
+        if (right < length && arr[right] > arr[largest]) 
+        largest = right; 
+        if (largest != i) { 
+            Swap(arr, i, largest);
+            Heapify(arr, length, largest); 
+        } 
+    }
+
+    public static void Swap(int arr[], int i, int j) {
+        arr[i] = (arr[i] + arr[j]) - (arr[j] = arr[i]);
+    }
+
+    static void PrintArray(int arr[]) {
+        for (int i = 0; i < arr.length; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
     public static void main(String[] args) {
-        int n;
-        int[] arr = new arr[length];
-        System.out.println(Sort(arr));
+        int arr[] = {12, 11, 13, 5, 6, 7};
+  
+        HeapSort ob = new HeapSort(); 
+        ob.Sort(arr); 
+  
+        System.out.println("Sorted array is"); 
+        PrintArray(arr); 
     }
 }
