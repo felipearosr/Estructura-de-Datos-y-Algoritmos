12ca55511f0a0ecca6fb115806a9acbb811e22ad
Felipe Aros <felipearos@pop-os.localdomain>
2020-05-12 02:24:49 -0400

    .

--- laboratorios/lab3/InsertSort.java
+++ laboratorios/lab3/InsertSort.java
@@ -1,3 +1,22 @@
 public class InsertSort {
     
+    void sort(int arr[]) {
+        for (int i = 1; i < arr.length; ++i) { 
+            int key = arr[i], 
+                j = i - 1; 
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = key; 
+        } 
+    }
+    public static void main(String args[]) { 
+        int arr[] = { 12, 11, 13, 5, 6 }; 
+      
+        InsertSort ob = new InsertSort(); 
+        ob.sort(arr); 
+      
+        Util.PrintArray(arr); 
+    } 
 }
